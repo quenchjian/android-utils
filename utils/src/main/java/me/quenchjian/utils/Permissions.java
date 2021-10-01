27677d622ebb3f4d84b7f73e3eeb499a7643f596
quenchjian <quench.jian@gmail.com>
2021-10-01 11:24:00 +0800

    Refactor: change request permission utility with ActivityResultContract

--- utils/src/main/java/me/quenchjian/utils/Permissions.java
+++ utils/src/main/java/me/quenchjian/utils/Permissions.java
@@ -7,6 +7,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.activity.ComponentActivity;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -18,8 +20,12 @@ import androidx.fragment.app.FragmentManager;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
+@SuppressWarnings("unused")
 public final class Permissions {
 
   private static final String TAG = "Permissions";
@@ -27,47 +33,46 @@ public final class Permissions {
   private static final List<String> EMPTY = Collections.emptyList();
 
   public static void request(Fragment fragment, Callback callback, String... perms) {
+    Objects.requireNonNull(fragment);
+    Objects.requireNonNull(callback);
     request(fragment.requireActivity(), callback, perms);
   }
 
   public static void request(Activity activity, Callback callback, String... perms) {
-    if (Build.VERSION.SDK_INT < REQ_CODE || check(activity, perms)) {
+    Objects.requireNonNull(activity);
+    Objects.requireNonNull(callback);
+    if (check(activity, perms)) {
       callback.onPermissionResult(Arrays.asList(perms), EMPTY, EMPTY);
       return;
     }
-    if (activity instanceof FragmentActivity) {
-      FragmentManager fm = ((FragmentActivity) activity).getSupportFragmentManager();
-      PermissionFragment frag = (PermissionFragment) fm.findFragmentByTag("PermissionFragment");
-      if (frag == null) {
-        frag = new PermissionFragment();
-        fm.beginTransaction().add(frag, "PermissionFragment").commitNow();
-      }
-      frag.request(callback, perms);
+    if (activity instanceof ComponentActivity) {
+      ((ComponentActivity) activity)
+          .registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+            RequestResult r = parseResult(activity, result);
+            callback.onPermissionResult(r.granted, r.denied, r.permanentDenied);
+          })
+          .launch(perms);
     } else {
-      Log.w(Permissions.TAG,
-          "Must call Permissions.onResult to handle result in Activity.onPermissionResult method");
+      Log.w(Permissions.TAG, "Must call Permissions.onResult to handle result in Activity.onPermissionResult method");
       ActivityCompat.requestPermissions(activity, perms, REQ_CODE);
     }
   }
 
-  public static void onResult(Activity activity, Callback callback, String[] perms) {
-    List<String> granted = new ArrayList<>();
-    List<String> denied = new ArrayList<>();
-    List<String> permanentDenied = new ArrayList<>();
-    for (String perm : perms) {
-      if (ContextCompat.checkSelfPermission(activity, perm) == PackageManager.PERMISSION_GRANTED) {
-        granted.add(perm);
-      } else {
-        denied.add(perm);
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, perm)) {
-          permanentDenied.add(perm);
-        }
-      }
+  public static void onResult(Activity activity, Callback callback, String[] perms, int[] grantResults) {
+    Objects.requireNonNull(activity);
+    Objects.requireNonNull(callback);
+    Objects.requireNonNull(perms);
+    Objects.requireNonNull(grantResults);
+    Map<String, Boolean> result = new HashMap<>();
+    for (int i = 0; i < perms.length; i++) {
+      result.put(perms[i], grantResults[i] == PackageManager.PERMISSION_GRANTED);
     }
-    callback.onPermissionResult(granted, denied, permanentDenied);
+    RequestResult r = parseResult(activity, result);
+    callback.onPermissionResult(r.granted, r.denied, r.permanentDenied);
   }
 
   private static boolean check(Context context, String... perms) {
+    Objects.requireNonNull(context);
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
       return true;
     }
@@ -79,35 +84,37 @@ public final class Permissions {
     return true;
   }
 
+  @NonNull
+  private static RequestResult parseResult(Activity activity, Map<String, Boolean> result) {
+    Objects.requireNonNull(activity);
+    Objects.requireNonNull(result);
+    RequestResult r = new RequestResult();
+    for (String perm : result.keySet()) {
+      if (result.get(perm) == Boolean.TRUE) {
+        r.granted.add(perm);
+      } else {
+        r.denied.add(perm);
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, perm)) {
+          r.permanentDenied.add(perm);
+        }
+      }
+    }
+    return r;
+  }
+
   private Permissions() {
     throw new RuntimeException("prevent reflection");
   }
 
   public interface Callback {
 
-    void onPermissionResult(List<String> granted, List<String> denied,
-        List<String> permanentDenied);
+    void onPermissionResult(List<String> granted, List<String> denied, List<String> permanentDenied);
   }
 
-  public static class PermissionFragment extends Fragment {
+  private static class RequestResult {
 
-    private Callback callback;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setRetainInstance(true);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-        @NonNull int[] grantResults) {
-      Permissions.onResult(requireActivity(), callback, permissions);
-    }
-
-    private void request(Callback callback, String... perms) {
-      this.callback = callback;
-      ActivityCompat.requestPermissions(requireActivity(), perms, REQ_CODE);
-    }
+    final List<String> granted = new ArrayList<>();
+    final List<String> denied = new ArrayList<>();
+    final List<String> permanentDenied = new ArrayList<>();
   }
 }
